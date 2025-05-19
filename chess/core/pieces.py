95c4f70b2aa586ac27cccbea3c64d541dd62352a
Marlon Miguel <marlonmiguel01@outlook.com>
2025-05-20 01:14:41 +0200

    added position as dataclass

--- chess/core/pieces.py
+++ chess/core/pieces.py
@@ -1,6 +1,13 @@
+from dataclasses import dataclass
 from enum import Enum
 
 
 class PieceColour(Enum):
     WHITE = 0
     BLACK = 1
+
+
+@dataclass
+class Position:
+    file: str  # a-h
+    rank: int  # 1-8
