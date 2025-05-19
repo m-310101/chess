29d01a913383a05f5abc2db176ac44182ff5df27
Marlon Miguel <marlonmiguel01@outlook.com>
2025-05-20 00:46:37 +0200

    changed entrypoint for cli

--- chess/__main__.py
+++ chess/__main__.py
@@ -1,6 +1,6 @@
 """Entry point for chess."""
 
-from chess.cli import main  # pragma: no cover
+from .ui.cli import main  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
     main()
